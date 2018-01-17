659bcaca0e26dfa2768f110a33733653e14e42cd
Kyle Harris <k_harris95@live.com>
2018-01-17 18:55:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,16 @@
 #write your code here
-def countdown_with_sleep
-
+def countdown_with_sleep(n)
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep(n)
+  end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown(n)
   until n == 0
-    puts "#{n} SECOND (S)!"
+    puts "#{n} SECOND(S)!"
     n -= 1
   end
   return "HAPPY NEW YEAR!"
